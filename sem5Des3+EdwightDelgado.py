2f37d190e1a9b5a6b43f6c9ec929339b5242cf93
edwight <edwardelgado0@gmail.com>
2013-05-24 14:33:09 -0300

    Sem5Des3+EdwightDelgado.py
    programa que imprime un texto en pantalla triangularmente

---
+++ sem5Des3+EdwightDelgado.py
@@ -0,0 +1,10 @@
+#coding:utf-8
+# lo hice de esta manera porque desconozco otra forma
+suma = []
+valor = 'grupo de estudio'
+for x in valor:
+    if x > 1:
+        suma.append(x)
+        print suma
+
+
